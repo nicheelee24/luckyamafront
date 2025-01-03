a30ba49bed2f7f80c86dc053d552e2631d3fabac
koiescafe2024 <nicheelee24@gmail.com>
2025-01-03 13:11:59 +0530

    Update Deposit.js

--- src/components/transfer/Deposit.js
+++ src/components/transfer/Deposit.js
@@ -108,7 +108,7 @@ export default function Deposit({ open, setOpen, type, setType }) {
             url = process.env.REACT_APP_BACKEND + "/api/pay/deposit_bigpay_bank";
             data=[
                 amount=amountRef.current.value,
-                bbn=bbnRef.current.value,
+                //bbn=bbnRef.current.value,
                 currency="baht",
                 platform=process.env.REACT_APP_PLATFORM
              ]
